29baadcc76181820a26cedee53f205c939841364
quochoi2 <email@moi.com>
2025-02-04 13:36:04 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,17 @@ export default defineConfig(({ command, mode }) => {
     server: {
       port: Number(env.VITE_PORT) || 5506,
     },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+        },
+      },
+    },
   };
 });
+
